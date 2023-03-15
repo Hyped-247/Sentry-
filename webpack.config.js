4ea542fa27e1249044b9de9507671ac160c70943
Francis Wang <franciswang@NJKNHVVGV9.local>
2023-03-15 00:29:18 -0400

    setup webpack plugin + create error button on home page

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const SentryWebpackPlugin = require('@sentry/webpack-plugin');
 
 module.exports = (env) => {
     const isProduction = env === 'production';
     const CSSExtract = new ExtractTextPlugin('styles.css');
     return {
         entry: path.join(__dirname, '/frontend/src/app.js'),
+        devtool: "source-map",
         output: {
             path: path.join(__dirname, '/frontend/static/frontend/'),
             filename: 'bundle.js',
@@ -41,8 +43,15 @@ module.exports = (env) => {
         resolve: {
             alias: { cldr$: 'cldrjs', cldr: 'cldrjs/dist/cldr' },
         },
-        plugins: [CSSExtract],
-        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        plugins: [CSSExtract,
+            new SentryWebpackPlugin({
+                release: "testRelease",
+                org: "ORGg",
+                project: "PROJECT",
+                authToken: 'AUTHTOKEN',
+                urlPrefix: "~/static"
+              })],
+        // devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer: {
             contentBase: path.join(__dirname, '/frontend/static/frontend/'),
             historyApiFallback: true,
